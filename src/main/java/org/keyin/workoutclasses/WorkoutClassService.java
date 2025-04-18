f9b13586dee476eea0a3f7193ddf872de8d0233a
Cody Snow <codysnow3@gmail.com>
2025-04-18 20:04:29 -0230

    WorkoutClassService method typos fixed

--- src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
+++ src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
@@ -4,6 +4,10 @@ import java.sql.SQLException;
 
 public class WorkoutClassService {
     // Inject in the DAO to be able to use it in the service
-    private WorkoutClassDAO workoutDAO;
+    private WorkoutClassDAO workoutDAO WorkoutClassDAO = new WorkoutClassDAO;
+
+    public void addWorkoutClass (WorkoutClass workoutClass) throws SQLException {
+        WorkoutClassDAO.addWorkoutClass (workoutClass);
+    }
 
 }
