219794e6015ec36194708031a586dd82cbccb5ed
Cody Snow <codysnow3@gmail.com>
2025-04-17 23:08:23 -0230

    Added  MembershipService with addMembership method

--- src/main/java/org/keyin/memberships/MembershipService.java
+++ src/main/java/org/keyin/memberships/MembershipService.java
@@ -1,11 +1,17 @@
 package org.keyin.memberships;
 
-
+import java.util.List;
+import java.sql.SQLException;
 // Service class for membership handle all the business logic
 // and only uses the DAO to interact with the database it does not have methods to do so
 // you can inject in your dao to use in your service. An example will be in the code
 public class MembershipService {
 
+    private final MembershipDAO dao = new MembershipDAO();
+
+    public void addMembership (Membership membership) throws SQLException {
+        dao.addMembership(membership);
+    }
     // When you inject in the DAO you have access to all methods in it
     //MembershipDAO dao = new MembershipDAO();
 
