e88178a57fcec325c67d97f9725040e56d94d2fc
Cody Snow <codysnow3@gmail.com>
2025-04-18 21:42:21 -0230

    Added missing import and finalized WorkoutClassService logic

--- src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
+++ src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
@@ -1,13 +1,18 @@
 package org.keyin.workoutclasses;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class WorkoutClassService {
     // Inject in the DAO to be able to use it in the service
-    private WorkoutClassDAO workoutDAO WorkoutClassDAO = new WorkoutClassDAO;
+    private final WorkoutClassDAO workoutDAO = new WorkoutClassDAO();
 
     public void addWorkoutClass (WorkoutClass workoutClass) throws SQLException {
-        WorkoutClassDAO.addWorkoutClass (workoutClass);
+        workoutDAO.addWorkoutClass (workoutClass);
+    }
+
+    public List<WorkoutClass> getAllWorkoutClasses() throws SQLException {
+        return workoutDAO.getAllWorkoutClasses();
     }
 
 }
